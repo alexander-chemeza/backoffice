cea63842f1301e0450884d18636d97a8c9fc2970
alexander-chemeza <alexander.chemeza@gmail.com>
2024-09-23 19:57:45 +0300

    UPDATE: changed language setting and added login_redirect_url

--- backoffice_crm/backoffice_crm/settings.py
+++ backoffice_crm/backoffice_crm/settings.py
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
@@ -135,3 +135,5 @@ MEDIA_ROOT = BASE_DIR / 'uploads'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
